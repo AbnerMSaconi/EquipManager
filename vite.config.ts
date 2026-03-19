191d8e62224f82132e42befc3f2474fc233fc04c
Abner Saconi <abnermatheusmoraes@gmail.com>
2026-03-19 15:59:11 -0400

    removido imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    plugins: [
-      react(), 
-      tailwindcss(),
-      basicSsl()
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+import {defineConfig} from 'vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    server: {
-      host: true, // 3. Permite que o celular acesse a aplicação via IP (ex: 192.168.x.x)
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modify—file watching is disabled to prevent flickering during agent edits.
-      hmr: process.env.DISABLE_HMR == 'true',
-    },
-  };
+  },
+  server: {
+    host: true, // Lembra-te de manter isto se precisares de acessar o servidor a partir de outros dispositivos na rede local
+  },
 });
