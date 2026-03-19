99721271f31724fedbd748fb15d81a89d4447c0a
Abner Saconi <abnermatheusmoraes@gmail.com>
2026-03-19 15:57:09 -0400

    Removido apis do google ai studio

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,25 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-
+import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
+    plugins: [
+      react(), 
+      tailwindcss(),
+      basicSsl()
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
     },
     server: {
+      host: true, // 3. Permite que o celular acesse a aplicação via IP (ex: 192.168.x.x)
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
-      hmr: process.env.DISABLE_HMR !== 'true',
+      // Do not modify—file watching is disabled to prevent flickering during agent edits.
+      hmr: process.env.DISABLE_HMR == 'true',
     },
   };
 });
